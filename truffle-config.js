747915269a234ca03999c2fdb76b759d9406abb0
Mohit Chandel <45650327+mohitchandel@users.noreply.github.com>
2022-03-09 16:31:56 +0530

    deployed on rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,29 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const infuraKey = "0d2ce437ceee43b8a4d66d6d01e634c9";
 
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = require("./secret.json").mnemonic;
 
 module.exports = {
 
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*",
     },
     rinkeby: {
-      privider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/0d2ce437ceee43b8a4d66d6d01e634c9`),
       network_id: 4,
       gas: 5500000,
-      gasPrice: 5000000000
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.0",
+      version: "0.8.12",
       optimizer: {
         enabled: false,
         runs: 200,
